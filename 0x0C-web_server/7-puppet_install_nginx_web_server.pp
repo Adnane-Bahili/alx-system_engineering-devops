d5acf05d7af4c3ebf6d88134309f3e6255096c89
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-09-27 00:44:51 +0000

    Puppet, fixing output error

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,9 @@
 # Installs an Nginx server using Puppet
-
-exec {'install nginx':
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx,
-}
-exec {'main page string'
-path       => '/var/www/nginx/html'
-command    => "/bin/echo 'Hello World!' | sudo tee /var/www/nginx/html/index.html",
-}
-exec { 'default config':
-  path     => '/etc/nginx/sites-available',
-  command  => "/bin/echo 'server {
-  listen 80;
-  listen [::]:80;
-  server_name servingweb.tech;
-  root /var/www/nginx/html;
-  index index.html;
-  location /redirect_me {
-    return 301 https://en.wikipedia.org/wiki/Nginx;
-  }
-  error_page 404 /404.html;
-  location /404 {
-    root /etc/nginx/html;
-    internal;
-  }
-}' > /etc/nginx/sites-available/default",
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx;\
+  echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;\
+  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me\
+  https:\/\/en.wikipedia.org\/wiki\/Nginx permanent;/" /etc/nginx/sites-available/default;\
+  sudo service nginx start',
 }
